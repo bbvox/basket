671c26010b3e61dddd028fceb3c34fc45d1f3ac1
Boyan Bakov <boyan.bakov@endava.com>
2021-08-11 19:18:44 +0300

    Cover: order.service

--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,16 +1,100 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OrderService } from './order.service';
+import { Order, Product, Unit } from '@app/models';
 
 describe('OrderService', () => {
   let service: OrderService;
+  const productMock: Product = {
+    id: '11111',
+    name: 'Test product',
+    position: 1,
+    available: true,
+    price: 0.8,
+    unit: Unit.LOAF,
+  };
+  const productDiscountMock: Product = {
+    id: '11112',
+    name: 'Test product with discount',
+    position: 1,
+    available: true,
+    price: 0.8,
+    unit: Unit.BAG,
+  };
+  const ordersExpect: Order[] = [
+    {
+      id: '22222-1',
+      name: 'Test product',
+      productId: '11111',
+      price: 0.8,
+      unit: Unit.LOAF,
+      total: 1,
+      priceRate: 100,
+      totalPrice: 0.8,
+    },
+  ];
+  const ordersDiscountExpect = [
+    {
+      id: '22222-1',
+      name: 'Test product with discount',
+      productId: '11112',
+      price: 0.8,
+      unit: Unit.BAG,
+      total: 1,
+      priceRate: 90,
+      totalPrice: 0.72,
+    },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(OrderService);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add Product to Orders without discount', () => {
+    service.add(productMock);
+
+    service.getAsObservable().subscribe((orders) => {
+      expect(orders).toEqual(ordersExpect);
+      expect(orders[0].priceRate).toEqual(100);
+    });
+  });
+
+  it('should add Product to Orders with discount', () => {
+    service.add(productDiscountMock);
+
+    service.getAsObservable().subscribe((orders) => {
+      expect(orders).toEqual(ordersDiscountExpect);
+      expect(orders[0].priceRate).toEqual(90);
+    });
+  });
+
+  it('should add new Product to Orders with discount', () => {
+    service.add(productMock);
+    service.add(productDiscountMock);
+
+    service.getAsObservable().subscribe((orders) => {
+      expect(orders).toEqual([
+        ...ordersExpect,
+        { ...ordersDiscountExpect[0], id: '22222-2' },
+      ]);
+    });
+  });
+
+  it('should increment total and totalPrice - existing product', () => {
+    service.add(productDiscountMock);
+    service.add(productDiscountMock);
+
+    service.getAsObservable().subscribe((orders) => {
+      const ordersExpect = [
+        { ...ordersDiscountExpect[0], total: 2, totalPrice: 1.44 },
+      ];
+      expect(orders).toEqual(ordersExpect);
+      expect(orders[0].priceRate).toEqual(90);
+    });
+  });
 });
--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { ProductService } from './product.service';
+import { Product, Unit } from '@app/models';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let result: Product[];
+  let httpTestingController: HttpTestingController;
+  const productIdMock = '11111';
+  const resultsMock = [
+    {
+      id: '11111',
+      name: 'Test data',
+      position: 1,
+      available: true,
+      price: 0.8,
+      unit: Unit.LOAF,
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService],
+    });
     service = TestBed.inject(ProductService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('expect http request with Product result', () => {
+    service.get().subscribe((products) => (result = products));
+
+    const req = httpTestingController.expectOne('assets/json/products.json');
+    expect(req.request.method).toEqual('GET');
+    req.flush(resultsMock);
+
+    expect(result).toEqual(resultsMock);
+  });
+
+  it('expect to getById return exact product', () => {
+    service.get().subscribe((products) => (result = products));
+
+    const req = httpTestingController.expectOne('assets/json/products.json');
+    req.flush(resultsMock);
+
+    const product = service.getById(productIdMock);
+    expect(product).toEqual(resultsMock[0]);
+  });
 });
